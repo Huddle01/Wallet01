8aac81b6c9b826e808287a4818363ed120aee7c3
deepso <deeps.o7o8.y@gmail.com>
2022-08-08 22:24:14 +0530

    add base class for connectors and tsconfig

---
+++ packages/core/src/constants/index.ts
@@ -0,0 +1,19 @@
+export type { BlockExplorer, BlockExplorerName } from "./blockExplorers";
+export { etherscanBlockExplorers } from "./blockExplorers";
+
+export {
+  alchemyRpcUrls,
+  defaultAlchemyApiKey,
+  defaultInfuraApiKey,
+  infuraRpcUrls,
+  publicRpcUrls,
+} from "./rpcs";
+export type { RpcProviderName } from "./rpcs";
+
+export {
+  chain,
+  chainId,
+  allChains,
+  defaultChains,
+  defaultL2Chains,
+} from "./chains";
