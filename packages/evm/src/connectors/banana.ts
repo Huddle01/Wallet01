3b06fac83b66d98e30a8ba0da10e155316f187a0
nlok5923 <nlokhande5923@gmail.com>
2023-04-16 23:58:24 +0530

    integrated banana wallet

--- packages/evm/src/connectors/banana.ts
+++ packages/evm/src/connectors/banana.ts
@@ -4,7 +4,7 @@ import {
   Banana,
   Chains as BananaSupportedChains,
   Wallet,
-} from "@rize-labs/banana-wallet-sdk/dist";
+} from "@rize-labs/banana-wallet-sdk";
 
 export class BananaConnector extends BaseConnector<Banana4337Provider> {
   provider!: Banana4337Provider;
@@ -47,15 +47,12 @@ export class BananaConnector extends BaseConnector<Banana4337Provider> {
   async connect(): Promise<void> {
     const walletName = this.BananaInstance.getWalletName();
     let provider;
-    // let walletAddress = "";
-    //@ts-ignore
-    this?.emit("message", { type: "connecting" });
+
     if (walletName) {
       this.wallet = await this.BananaInstance.connectWallet(walletName);
       if (!this.wallet) {
         throw new Error("Wallet connection failed");
       }
-    //   walletAddress = await this.wallet.getAddress();
       provider = this.wallet.getProvider();
     } else {
       // @ts-ignore-next-line
@@ -63,7 +60,7 @@ export class BananaConnector extends BaseConnector<Banana4337Provider> {
       if (!this.wallet) {
         throw new Error("Wallet creation failed");
       }
-    //   walletAddress = await this.wallet.getAddress();
+
       provider = this.wallet.getProvider();
     }
     setLastUsedConnector(this.name);
@@ -78,21 +75,36 @@ export class BananaConnector extends BaseConnector<Banana4337Provider> {
 
   async resolveDid(address: string): Promise<string | null> {
     try {
-        console.log("Not implemented yet");
-        console.log(address)
-        return "none";     
+      if (!this.provider) throw new Error("Wallet not connected");
+      if ((await this.getChainId()) !== "1") return null;
+      const name = this.provider.lookupAddress(address);
+      return name;
     } catch (error) {
       console.error(error);
       throw error;
     }
   }
 
+  async switchChain(chainId: string): Promise<void> {
+    const isNewChainSupported = isBananSupported(chainId);
+    if (!isNewChainSupported)
+      throw new Error(`Unsupported chainId: ${chainId}`);
+
+    const bananaChain = getBananaSupportedChain(chainId);
+
+    const BananaInstance = new Banana(bananaChain);
+    this.BananaInstance = BananaInstance;
+
+    // connect to same wallet with new configs
+    await this.connect();
+  }
+
   async signMessage(message: string): Promise<string> {
     try {
       if (!this.provider) throw new Error("Wallet not Connected!");
       const signer = this.wallet.getSigner();
-
       const hash = (await signer.signBananaMessage(message)).signature;
+
       return hash;
     } catch (error) {
       console.error(error);
@@ -113,68 +125,70 @@ export class BananaConnector extends BaseConnector<Banana4337Provider> {
   }
 
   async disconnect() {
-    console.log("disconnect called");
     this.BananaInstance.resetWallet();
   }
 
-  //! Current Banana wallet has 1:1 mapping with accounts
+  async getChainId(): Promise<string> {
+    if (this.provider) {
+      const id = this.provider.chainId;
+      this.chain = id.toString();
+      return this.chain;
+    }
+    return "";
+  }
+
+  //! Currently Banana wallet has 1:1 mapping with accounts
   protected onAccountsChanged(): void {
     throw new Error("Method not implemented.");
   }
 
   protected onChainChanged(chain: string | number): void {
-    const id = normalizeChainId(chain);
-    const isNewChainSupported = isBananSupported(id);
-
-    // @ts-ignore-next-line
-    this?.emit("change", { chain: { id, isNewChainSupported } });
+    console.log("Chain changed to ", chain);
   }
 
   protected onDisconnect(): void {
-    console.log("disconnect called ");
     this.BananaInstance.resetWallet();
-    // @ts-ignore-next-line
-    this?.emit("disconnect");
   }
 }
-
 
 function isBananSupported(chainId: number | string): boolean {
-    for (let chain in BananaSupportedChains) {
-      let currentChainId = BananaSupportedChains[chain] || -1;
-      if (currentChainId.toString() == chainId) return true;
-    }
-    return false;
+  chainId = normalizeChainId(chainId);
+  for (let chain in BananaSupportedChains) {
+    let currentChainId = BananaSupportedChains[chain] || -1;
+    if (currentChainId == chainId) return true;
   }
-
-function getBananaSupportedChain(chainId: number | string): BananaSupportedChains {
-    const normalizedChainId = normalizeChainId(chainId);
-    let bananaSupportedChain: BananaSupportedChains;
-    switch(normalizedChainId) {
-        case 80001:
-            bananaSupportedChain = BananaSupportedChains.mumbai
-            break;
-        case 420:
-            bananaSupportedChain = BananaSupportedChains.optimismTestnet
-            break;
-        case 5:
-            bananaSupportedChain = BananaSupportedChains.goerli;
-            break;
-        default:
-            bananaSupportedChain = BananaSupportedChains.mumbai;
-            break;
-    };
-
-    return bananaSupportedChain;
+  return false;
 }
 
+function getBananaSupportedChain(
+  chainId: number | string
+): BananaSupportedChains {
+  const normalizedChainId = normalizeChainId(chainId);
+  let bananaSupportedChain: BananaSupportedChains;
+  switch (normalizedChainId) {
+    case 80001:
+      bananaSupportedChain = BananaSupportedChains.mumbai;
+      break;
+    case 420:
+      bananaSupportedChain = BananaSupportedChains.optimismTestnet;
+      break;
+    case 5:
+      bananaSupportedChain = BananaSupportedChains.goerli;
+      break;
+    default:
+      bananaSupportedChain = BananaSupportedChains.mumbai;
+      break;
+  }
+
+  return bananaSupportedChain;
+}
 
 function normalizeChainId(chainId: string | number | bigint) {
-    if (typeof chainId === "string")
-      return Number.parseInt(
-        chainId,
-        chainId.trim().substring(0, 2) === "0x" ? 16 : 10
-      );
-    if (typeof chainId === "bigint") return Number(chainId);
-    return chainId;
-  }
+  if (typeof chainId === "string")
+    return Number.parseInt(
+      chainId,
+      chainId.trim().substring(0, 2) === "0x" ? 16 : 10
+    );
+  if (typeof chainId === "bigint") return Number(chainId);
+  return chainId;
+}
