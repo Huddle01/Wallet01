530058935f42fbbe8c6dc03cbbb7069de7e2fe33
vatsal <0505m2003@gmail.com>
2023-06-26 20:59:27 +0530

    create a simpler example

--- examples/nextjs/pages/_app.tsx
+++ examples/nextjs/pages/_app.tsx
@@ -2,36 +2,22 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 import { Wallet01 } from "@wallet01/react";
-import {
-  CoinbaseConnector,
-  InjectedConnector,
-  WalletconnectConnector,
-} from "@wallet01/evm";
-import { KeplrConnector } from "@wallet01/cosmos";
-import { PhantomConnector, SolflareConnector } from "@wallet01/solana";
-import { TempleConnector } from "@wallet01/tezos";
+import { InjectedConnector, WalletconnectConnector } from "@wallet01/evm";
 
 import Layout from "../components/layout";
-import { FilecoinConnector } from "../lib/filecoin";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Wallet01
       autoConnect={true}
       connectors={() => [
-        new FilecoinConnector(),
         new InjectedConnector(),
-        new CoinbaseConnector(),
         new WalletconnectConnector({
           chain: "1",
-          chains: [1, 4, 137],
+          chains: [1],
           projectId: "190ea8e3ce540de271817462e33e310b",
           showQrModal: true,
         }),
-        new PhantomConnector(),
-        new SolflareConnector(),
-        new KeplrConnector(),
-        new TempleConnector(),
       ]}
     >
       <Layout>
