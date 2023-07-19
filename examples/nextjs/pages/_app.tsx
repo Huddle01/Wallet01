e5b0c265099709939c66ee3a9defc903a5acf359
vatsal <0505m2003@gmail.com>
2023-07-19 21:42:09 +0530

    changing import for beacon

--- examples/nextjs/pages/_app.tsx
+++ examples/nextjs/pages/_app.tsx
@@ -11,7 +11,7 @@ import { Wallet01 } from "@wallet01/react";
 // } from "@wallet01/evm";
 // import { KeplrConnector } from "@wallet01/cosmos";
 // import { PhantomConnector, SolflareConnector } from "@wallet01/solana";
-import { BeaconConnector } from "@wallet01/tezos";
+import { BeaconConnector } from "@wallet01/tezos/dist/connectors/BeaconConnector";
 import Layout from "../components/layout";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -31,7 +31,7 @@ export default function App({ Component, pageProps }: AppProps) {
         // new PhantomConnector(),
         // new SolflareConnector(),
         // new KeplrConnector(),
-        // // new TempleConnector({ projectName: "Wallet01" }),
+        // new TempleConnector({ projectName: "Wallet01" }),
         // new BananaConnector(),
         // new OkxWalletConnector(),
         new BeaconConnector({ projectName: "Wallet01" }),
