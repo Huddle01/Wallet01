b38e1982da1fb7091373f4a54219dc9849ba5b1e
vatsal <0505m2003@gmail.com>
2023-07-19 09:47:49 +0530

    rm wc for testing

--- examples/nextjs/pages/_app.tsx
+++ examples/nextjs/pages/_app.tsx
@@ -5,7 +5,7 @@ import { Wallet01 } from "@wallet01/react";
 import {
   CoinbaseConnector,
   InjectedConnector,
-  WalletconnectConnector,
+  // WalletconnectConnector,
   BananaConnector,
   OkxWalletConnector,
 } from "@wallet01/evm";
@@ -20,13 +20,13 @@ export default function App({ Component, pageProps }: AppProps) {
       autoConnect={true}
       connectors={() => [
         new InjectedConnector(),
-        new WalletconnectConnector({
-          chain: "1",
-          chains: [1],
-          optionalChains: [137, 80001],
-          projectId: "cef77f9c3969fb563468d997449c92d2",
-          showQrModal: true,
-        }),
+        // new WalletconnectConnector({
+        //   chain: "1",
+        //   chains: [1],
+        //   optionalChains: [137, 80001],
+        //   projectId: "cef77f9c3969fb563468d997449c92d2",
+        //   showQrModal: true,
+        // }),
         new CoinbaseConnector(),
         new PhantomConnector(),
         new SolflareConnector(),
